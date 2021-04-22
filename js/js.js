af3a93b6a0590ba24ba6a71144792ba4ffffdef3
i-Taboo <82161706+i-Taboo@users.noreply.github.com>
2021-04-22 14:32:51 -0400

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,20 @@
+function cambiolista1() {
+    var reproductor = document.getElementById("reproductor");
+    reproductor.setAttribute("src","audio/lost in paradise lofi.mp3");
+}
+
+function cambiolista2() {
+    var reproductor = document.getElementById("reproductor");
+    reproductor.setAttribute("src","audio/lost in paradise.mp3");
+}
+
+function cambiolista3() {
+    var reproductor = document.getElementById("reproductor");
+    reproductor.setAttribute("src","audio/shiloh.mp3");
+}
+
+function cambiolista4() {
+    var reproductor = document.getElementById("reproductor");
+    reproductor.setAttribute("src","audio/fukashigi no krate.mp3");
+}
+
